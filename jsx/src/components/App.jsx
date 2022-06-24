80fa67343c2e8257d562332c8f8622324dd42275
Joanne Tang <joannetangrd@gmail.com>
2022-06-24 10:36:52 -0400

    animals componenet

--- jsx/src/components/App.jsx
+++ jsx/src/components/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 // Additional imports
+import Animals from './Animals';
 
 const App = () => {
   return (
     <div>
       <h1>Zoo Animals</h1>
+      <Animals />
     </div>
   );
 };
