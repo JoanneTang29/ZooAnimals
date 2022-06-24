19008d634e67975a877509fe86bd8552e2909597
Joanne Tang <joannetangrd@gmail.com>
2022-06-24 11:13:51 -0400

    animals comp fixed

--- jsx/src/components/Animals.jsx
+++ jsx/src/components/Animals.jsx
@@ -5,6 +5,11 @@ import axios from 'axios';
 const Animals = () => {
   // Create a variable to hold application state
   // Create effect
+  return (
+    <div>
+      <h1> Animals</h1>
+    </div>
+  );
 };
 
 export default Animals;
