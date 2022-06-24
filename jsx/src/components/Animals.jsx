f966d7a050a140a42b5131482be01bc10e755a22
Joanne Tang <joannetangrd@gmail.com>
2022-06-24 10:19:13 -0400

    src folder added

---
+++ jsx/src/components/Animals.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import axios from 'axios';
+
+const Animals = () => {
+  // Create a variable to hold application state
+  // Create effect
+};
+
+export default Animals;
---
+++ jsx/src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// Additional imports
+
+const App = () => {
+  return (
+    <div>
+      <h1>Zoo Animals</h1>
+    </div>
+  );
+};
+
+export default App;
